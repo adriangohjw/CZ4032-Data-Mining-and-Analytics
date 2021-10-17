0aed925076948a6119228e148ceaf1b179c94263
adriangohjw <adriangohjw@gmail.com>
2021-10-17 18:40:33 +0800

    Add improved association rule + display space-time cost for original script

--- association_rule/association_rule_improved.py
+++ association_rule/association_rule_improved.py
@@ -1,5 +1,6 @@
 from data_processor import load_dataset, filter_df_columns, append_col_name_to_dataframe, get_frequent_itemsets, get_association_rules
 from performance_analyzer import get_df_memory_size
+from performance_optimizer import get_optimized_df_and_dict, repopulate_assoc_rules
 import time
 
 DATA_SOURCE = 'data/adult_cleaned.csv'
@@ -13,11 +14,16 @@ data = load_dataset(DATA_SOURCE, DELIMITER)
 data_with_selected_attributes = filter_df_columns(data, attributes)
 df_with_appended_column_names = append_col_name_to_dataframe(data_with_selected_attributes)
 
-print(get_df_memory_size(df_with_appended_column_names))
+start = time.process_time()
+df_optimized, reference_dict = get_optimized_df_and_dict(df_with_appended_column_names)
+elapsed_time = time.process_time() - start
+print(">>>>> Time to determine optimize dataframe: " + str(elapsed_time) + "secs")
+print(get_df_memory_size(df_optimized))
 
 start = time.process_time()
 frequent_items = get_frequent_itemsets(df_with_appended_column_names, min_support=0.5)
 assoc_rules = get_association_rules(frequent_items)
+repopulate_assoc_rules(assoc_rules, reference_dict)
 elapsed_time = time.process_time() - start
 print(">>>>> Time to determine assoc rules: " + str(elapsed_time) + "secs")
 
