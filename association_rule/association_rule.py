a260f4ab1f52ee7a614a9794ac23bc05d979224c
adriangohjw <adriangohjw@gmail.com>
2021-10-17 16:42:38 +0800

    Break down data processor to more parts

--- association_rule/association_rule.py
+++ association_rule/association_rule.py
@@ -1,4 +1,4 @@
-from data_processor import load_dataset, get_frequent_itemsets, get_association_rules
+from data_processor import load_dataset, filter_df_columns, append_col_name_to_dataframe, get_frequent_itemsets, get_association_rules
 
 DATA_SOURCE = 'data/adult_cleaned.csv'
 DELIMITER = ','
@@ -8,6 +8,8 @@ attributes = [
 ]
 
 data = load_dataset(DATA_SOURCE, DELIMITER)
-frequent_items = get_frequent_itemsets(data, min_support=0.5, colnames=attributes)
+data_with_selected_attributes = filter_df_columns(data, attributes)
+df_with_appended_column_names = append_col_name_to_dataframe(data_with_selected_attributes)
+frequent_items = get_frequent_itemsets(df_with_appended_column_names, min_support=0.5)
 assoc_rules = get_association_rules(frequent_items)
 print(assoc_rules)
