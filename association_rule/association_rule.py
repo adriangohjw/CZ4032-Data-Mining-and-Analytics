aae3093ca2e30752ff3803bd867b693fbd5620e5
adriangohjw <adriangohjw@gmail.com>
2021-10-20 16:04:17 +0800

    Add documentation for improved association rule

--- association_rule/association_rule.py
+++ association_rule/association_rule.py
@@ -5,29 +5,28 @@ from rule_selection import RuleSelection
 from accuracy_analyzer import AccuracyAnalyzer
 from performance_analyzer import get_df_memory_size
 import time
+import pandas as pd
+
+# Suppress warnings
+pd.options.mode.chained_assignment = None
 
 #########################
 # Setting up and configurating which dataset and attributes to use
 #########################
 
 # DATA_SOURCE is the path to the dataset that we want to evaluate
-DATA_SOURCE = 'BreastCancer/data.csv'
+DATA_SOURCE = 'data/adult_cleaned.csv'
 DELIMITER = ','
 
 # These are all the attributes that will be used to perform the analysis
 attributes = [
-  'diagnosis', 'radius_mean', 'texture_mean', 'perimeter_mean', 'area_mean',
-  'smoothness_mean', 'compactness_mean', 'concavity_mean', 'concave points_mean',
-  'symmetry_mean', 'fractal_dimension_mean', 'radius_se', 'texture_se',
-  'perimeter_se', 'area_se', 'smoothness_se', 'compactness_se', 'concavity_se',
-  'concave points_se', 'symmetry_se', 'fractal_dimension_se', 'radius_worst',
-  'texture_worst', 'perimeter_worst', 'area_worst', 'smoothness_worst',
-  'compactness_worst', 'concavity_worst', 'concave points_worst', 'symmetry_worst',
-  'fractal_dimension_worst'
+  'age', 'hours-per-week',
+  'workclass', 'education', 'marital-status', 'occupation',
+  'relationship', 'race', 'sex', 'native-country', 'income-group'
 ]
 
 # This is the attribute to be predicted at this field
-attribute_to_classify = 'diagnosis'
+attribute_to_classify = 'income-group'
 
 
 #########################
@@ -74,7 +73,7 @@ start = time.process_time()
 #########################
 
 # Retrieve the frequent itemsets from the dataframe
-frequent_items = get_frequent_itemsets(df_with_appended_column_names, min_support=0.05)
+frequent_items = get_frequent_itemsets(df_with_appended_column_names, min_support=0.3)
 
 # Use the frequent itemsets to generate association rules based on Apriori algorithm
 assoc_rules = get_association_rules(frequent_items)
@@ -122,9 +121,9 @@ print()
 # Print out the classifiers generated
 #########################
 
-pprint(classifiers)
-print(len(classifiers))
-print()
+#pprint(classifiers)
+#print(len(classifiers))
+#print()
 
 
 #########################
